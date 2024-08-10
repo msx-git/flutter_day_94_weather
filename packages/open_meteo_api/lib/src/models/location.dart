737bbd69174bf17b5104498a73d2c8086dedfe35
Musoxon <msx.tuit@gmail.com>
2024-08-10 18:33:15 +0500

    open_meteo_api package is created.

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -1,3 +1,10 @@
+
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   const Location({
     required this.id,
@@ -10,4 +17,8 @@ class Location {
   final String name;
   final double latitude;
   final double longitude;
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+
 }
